a2564effc68f49f3c3b75f12c24c0ef9bd27907a
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-03-05 21:52:38 +0000

    Adapt tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 require('should');
 
+var runOperation = require('plumber-util-test').runOperation;
+
 var Resource = require('plumber').Resource;
 
 var hash = require('..');
@@ -36,35 +38,50 @@ describe('filter', function(){
   });
 
 
-  it('should produce a hash mapping', function(){
-    var output = hash()(resources);
-    output.length.should.equal(3);
-    output[2].filename().should.equal('assets-mapping.json');
+  describe('run on two resources', function() {
+    var output;
 
-    var mapping = JSON.parse(output[2].data());
-    Object.keys(mapping).length.should.equal(2);
-    mapping['file.js'].should.equal('file.4a04d50c.js');
-    mapping['file.css'].should.equal('file.2dcfb860.css');
-  });
+    beforeEach(function() {
+      output = runOperation(hash(), resources).resources;
+    });
 
-  it('should rename all input resources with their hash', function(){
-    var output = hash()(resources);
-    output[0].filename().should.equal('file.4a04d50c.js');
-    output[1].filename().should.equal('file.2dcfb860.css');
-  });
 
-  it('should preserve the same data for all input resources', function(){
-    var output = hash()(resources);
-    output[0].data().should.equal(resources[0].data());
-    output[1].data().should.equal(resources[1].data());
+    it('should produce a hash mapping', function(done){
+      output.toArray(function(hashedResources) {
+        hashedResources.length.should.equal(3);
+        hashedResources[2].filename().should.equal('assets-mapping.json');
+
+        var mapping = JSON.parse(hashedResources[2].data());
+        Object.keys(mapping).length.should.equal(2);
+        mapping['file.js'].should.equal('file.4a04d50c.js');
+        mapping['file.css'].should.equal('file.2dcfb860.css');
+        done();
+      });
+    });
+
+    it('should rename all input resources with their hash', function(){
+      output.toArray(function(hashedResources) {
+        hashedResources[0].filename().should.equal('file.4a04d50c.js');
+        hashedResources[1].filename().should.equal('file.2dcfb860.css');
+      });
+    });
+
+    it('should preserve the same data for all input resources', function(){
+      output.toArray(function(hashedResources) {
+        hashedResources[0].data().should.equal(resources[0].data());
+        hashedResources[1].data().should.equal(resources[1].data());
+      });
+    });
   });
 
 
   it('should produce an empty mapping if no resources as input', function(){
-    var output = hash()([]);
-    output.length.should.equal(1);
-    output[0].filename().should.equal('assets-mapping.json');
-    output[0].data().should.equal('{}');
+    var output = runOperation(hash(), []).resources;
+    output.toArray(function(hashedResource) {
+      hashedResource.length.should.equal(1);
+      hashedResource[0].filename().should.equal('assets-mapping.json');
+      hashedResource[0].data().should.equal('{}');
+    });
   });
 
 });
